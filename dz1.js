397dc516af852fc4972dd5802d91f26858b92eb3
Nurik <102224735+nurikgentle@users.noreply.github.com>
2022-06-21 19:44:28 +0600

    Add files via upload

---
+++ dz1.js
@@ -0,0 +1,16 @@
+// const first = [1, 2, 3, 4, 5]
+
+// const second = [6, 7, 8, 9, 10]
+
+// const third = [11, 12, 13, 14, 15]
+
+// const fourth = [16, 17, 18, 19, 20]
+
+// const fifth = [21, 22, 23, 24, 25]
+
+// let together = [first, second, third, fourth, fifth]
+
+// console.log(together);
+
+
+
---
+++ dz2.js
@@ -0,0 +1,15 @@
+// let person = {
+//     name: 'Noname',
+//     quality: 'Shitty',
+//     nickname: 'Dumb',
+//     age: '100',
+//     state: 'Unmarried'
+// }
+
+// let newPerson = {
+//     education: 'Limited with school',
+//     family: 'Does not exist',
+//     hobby: 'to do nothing',
+//     person
+// }
+// console.log(newPerson);
